06070ec86c7a38fb284dee8a926c101de0d8c12b
K-Love <kevin@backstageinteractive.com>
2024-12-13 16:47:25 -0800

    Add core layout components and base styles

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import { motion, AnimatePresence } from 'framer-motion'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <AnimatePresence mode="wait">
+        <motion.main
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.3 }}
+          className="flex-grow"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
