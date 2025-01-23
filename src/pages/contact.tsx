3552405ae0ed3ceb45dc1a05563937df163a5211
K-Love <kevin@backstageinteractive.com>
2025-01-23 13:54:06 -0800

    feat: restore contact form with analytics and tracking features

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
+import ContactForm from '@/components/sections/contact/ContactForm'
 
 const ContactPage: NextPage = () => {
   return (
@@ -15,7 +16,7 @@ const ContactPage: NextPage = () => {
       <div className="pt-20 min-h-screen">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
           <h1 className="text-4xl md:text-5xl font-bold mb-8">Contact Us</h1>
-          {/* Contact form will be added later */}
+          <ContactForm />
         </div>
       </div>
     </>
